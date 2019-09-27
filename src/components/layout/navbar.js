f1a8673657ea56f6ece364cf7a3c8d07b0c90c18
manishydv18 <55871846+manishydv18@users.noreply.github.com>
2019-09-27 13:03:19 +0530

    change the previous name

--- src/components/layout/navbar.js
+++ src/components/layout/navbar.js
@@ -4,7 +4,7 @@ const Navbar = () => {
     return (
       <React.Fragment>
           <nav className="navbar navbar-dark bg-dark mb-5">
-              <span className = "navbar-brand mb-0 h1 mx-auto">Music Infi</span>
+              <span className = "navbar-brand mb-0 h1 mx-auto">Music Lyrics</span>
           </nav>
       </React.Fragment>
     )
